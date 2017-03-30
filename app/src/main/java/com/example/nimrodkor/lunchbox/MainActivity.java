5b70d47cce7913ed1fef3f249f4354edae190c96
Nimrod Kor <nimrod.kor@gmail.com>
2017-03-30 10:37:44 +0300

    initial commit

---
+++ app/src/main/java/com/example/nimrodkor/lunchbox/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.nimrodkor.lunchbox;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.fragment_login);
+        fragment.onActivityResult(requestCode, resultCode, data);
+    }
+}
